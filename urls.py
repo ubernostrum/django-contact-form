a724f5f9a5189dac51310c4457b836c3a314c285
ubernostrum <devnull@localhost>
2007-05-18 05:17:05 +0000

    And first draft of urlpatterns

---
+++ urls.py
@@ -0,0 +1,26 @@
+"""
+Example URLConf for a contact form.
+
+Because the ``contact_form`` view takes configurable arguments, it's
+recommended that you manually place it somewhere in your URL
+configuration with the arguments you want. If you just prefer the
+default, however, you can hang this URLConf somewhere in your URL
+hierarchy (for best results with the defaults, include it under
+``/contact/``).
+
+"""
+
+from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
+from contact_form.views import contact_form
+
+urlpatterns = patterns('',
+                       url(r'^contact/$',
+                           contact_form,,
+                           name='contact_form'),
+                       
+                       url(r'^contact/sent/$',
+                           direct_to_template,
+                           { 'template': 'contact/contact_form_sent.html' },
+                           name='contact_form_sent'),
+                       )
