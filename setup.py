3b03dda35a64377bc50f6de6a9f638dd3fd78e9a
ubernostrum <devnull@localhost>
2007-05-21 09:38:03 +0000

    Some more shuffling

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='contact_form',
+      version='0.1',
+      description='Generic contact-form application for Django',
+      author='James Bennett',
+      author_email='james@b-list.org',
+      url='http://code.google.com/p/django-contact-form/',
+      packages=['contact_form'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'],
+      )
