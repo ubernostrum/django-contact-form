69a44ee0aa60cc90f6a48bf840524439e7fd5baa
James Bennett <james@b-list.org>
2015-05-10 14:31:41 -0700

    1.7-ify the URLConf.

--- contact_form/urls.py
+++ contact_form/urls.py
@@ -14,13 +14,12 @@ from django.views.generic import TemplateView
 from contact_form.views import ContactFormView
 
 
-urlpatterns = patterns('',
-                       url(r'^$',
-                           ContactFormView.as_view(),
-                           name='contact_form'),
-                       url(r'^sent/$',
-                           TemplateView.as_view(
-                               template_name='contact_form/contact_form_sent.html'
-                               ),
-                           name='contact_form_sent'),
-                       )
+urlpatterns = [
+    url(r'^$',
+        ContactFormView.as_view(),
+        name='contact_form'),
+    url(r'^sent/$',
+        TemplateView.as_view(
+            template_name='contact_form/contact_form_sent.html'),
+        name='contact_form_sent'),
+]
