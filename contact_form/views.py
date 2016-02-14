5bbba9bdbbe280c6307e003ffbd17ef42c402af4
James Bennett <james@b-list.org>
2016-02-13 21:43:03 -0800

    form_invalid() bug was fixed in 1.9.1, note that.

--- contact_form/views.py
+++ contact_form/views.py
@@ -34,8 +34,9 @@ class ContactFormView(FormView):
         # undesirable for performance reasons.
         #
         # Manually implementing this method, and passing the form
-        # instance to get_context_data(), solves this issue (which
-        # will be fixed in Django 1.9.1 and Django 1.10).
+        # instance to get_context_data(), solves this issue (which was
+        # fixed in Django 1.9.1 and will not be present in Django
+        # 1.10).
         return self.render_to_response(self.get_context_data(form=form))
 
     def get_form_kwargs(self):
