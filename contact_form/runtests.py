7c148f88dbeec9ae1258e2504d4edab493d788d3
James Bennett <james@b-list.org>
2017-03-09 13:30:15 -0800

    Switch to Makefile testing.

--- contact_form/runtests.py
+++ contact_form/runtests.py
@@ -78,3 +78,7 @@ def run_tests():
     test_runner = TestRunner(verbosity=1, interactive=True)
     failures = test_runner.run_tests(['contact_form.tests'])
     sys.exit(bool(failures))
+
+
+if __name__ == '__main__':
+    run_tests()
