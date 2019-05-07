ef7f7d2492b6bdf9bed6aae2fa5aadc3dd2f3d30
James Bennett <james@b-list.org>
2019-05-07 16:39:25 -0400

    And enforce Python versions.

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(name='django-contact-form',
                    'Programming Language :: Python :: 3.6',
                    'Programming Language :: Python :: 3.7',
                    'Topic :: Utilities'],
+      python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
       install_requires=[
           'Django>=1.11',
       ],
