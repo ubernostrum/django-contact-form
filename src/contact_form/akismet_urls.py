9557cf65c20ea5574103c6cbb148d9f0a9d71860
James Bennett <james@b-list.org>
2020-02-09 06:25:11 -0800

    Fix the akismet URLs.

--- src/contact_form/akismet_urls.py
+++ src/contact_form/akismet_urls.py
@@ -7,7 +7,7 @@ include this URLConf somewhere in your URL hierarchy (for example, at
 
 """
 
-from django.conf.urls import url
+from django.urls import path
 from django.views.generic import TemplateView
 
 from .forms import AkismetContactForm
@@ -15,13 +15,11 @@ from .views import ContactFormView
 
 
 urlpatterns = [
-    url(
-        r"^$",
-        ContactFormView.as_view(form_class=AkismetContactForm),
-        name="contact_form",
+    path(
+        "", ContactFormView.as_view(form_class=AkismetContactForm), name="contact_form",
     ),
-    url(
-        r"^sent/$",
+    path(
+        "sent/",
         TemplateView.as_view(template_name="contact_form/contact_form_sent.html"),
         name="contact_form_sent",
     ),
