3c677cedae52f0d43bce369e1a66f602c49af84a
James Bennett <james@b-list.org>
2024-05-26 18:30:15 -0700

    Add SPDX comment in test runner script.

--- runtests.py
+++ runtests.py
@@ -3,6 +3,8 @@ A standalone test runner script, since we don't ship a Django manage.py file.
 
 """
 
+# SPDX-License-Identifier: BSD-3-Clause
+
 from django.core.management import execute_from_command_line
 
 
